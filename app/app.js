9d5daba46577c3be9b4710bd00537fe72ea0a3a4
Arth Modi <arthmodi36@gmail.com>
2019-12-11 16:22:49 +0530

    sixth commit

--- app/app.js
+++ app/app.js
@@ -19,30 +19,54 @@ myNinjaApp.run(function(){
 //this is how we declare our controllers
 myNinjaApp.controller('NinjaController', ['$scope', function( $scope ){
     $scope.message="Hey buddy!";
+
+    $scope.removeNinja = function(ninja){
+        var removedNinja = $scope.ninjas.indexOf(ninja);
+        $scope.ninjas.splice(removedNinja,1);
+    }
+
+    $scope.addNinja = function(){
+        $scope.ninjas.push({
+            name: $scope.newNinja.name,
+            belt: $scope.newNinja.belt,
+            rate: parseInt($scope.newNinja.rate),
+            available: true
+        });
+
+        $scope.newNinja.name = "";
+        $scope.newNinja.belt = "";
+        $scope.newNinja.rate = "";
+
+    }
+
     $scope.ninjas=[
         {
             name: "Yoshi",
             belt: "green",
             rate: 50,
-            available:true
+            available: true,
+            thumb: "content/img/yoshi.jpg"
         },
         {
             name: "ryu",
             belt: "black",
             rate: 30,
-            available:true
+            available: true,
+            thumb: "content/img/ryu.png"
         },
         {
             name: "crystal",
             belt: "yellow",
             rate: 10,
-            available:false
+            available: false,
+            thumb: "content/img/crystal.jpg"
         },
         {
             name: "shaun",
             belt: "orange",
             rate: 20,
-            available:true
+            available: true,
+            thumb: "content/img/shaun.jpg"
         }
     ];
 }]);
